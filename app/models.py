b73525e2cff68945a3e9e1cea93b9c212a4a5ad4
Vishnu Bhargav <vishnubhargavsitra@gmail.com>
2026-01-07 12:22:44 +0530

    feat(phase-1): setup database connection and ORM models

---
+++ app/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from .database import Base
+
+class User(Base):
+    __tablename__="users"
+
+    #Columns
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True , index=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    created_at = Column(DateTime,default=datetime.now())
+
+    #Relationships
+    documents = relationship("Document", back_populates="owner", cascade="all, delete-orphan")
+
+
+class Document(Base):
+    __tablename__="documents"
+
+    #Columns
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100),nullable=False)
+    content = Column(Text, nullable=True)
+    created_at = Column(DateTime,default=datetime.now())
+
+    #Foreign Key
+    user_id = Column(Integer,ForeignKey("users.id"),nullable=False)
+
+    #Relationships
+    owner = relationship("User", back_populates="documents")
+
+
+
